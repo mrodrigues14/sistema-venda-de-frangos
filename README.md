c4a5782f1c1da8f7c32dea4bb1d126c013468882
Matheus Rodrigues <102708746+mrodrigues14@users.noreply.github.com>
2023-11-13 13:51:49 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# sistema-venda-de-frangos
+# Sistema de Vendas de Frango Assado
 Repositório dedicado ao projeto de um sistema de controle de uma empresa de vendas de frango assado.
---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
